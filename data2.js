8344555522bd20a653cc45fc2e3e9fa317b4315f
nayeye13 <156628256+nayeye13@users.noreply.github.com>
2024-04-12 16:26:14 +0900

    24.04.12

---
+++ data2.js
@@ -0,0 +1,233 @@
+let data2 = [
+
+         {
+
+                  id: 0,
+
+                  title: "MM101 R5",
+
+                  imgUrl: "/img/data2/11001_MM101_R5_3_1.avif",
+
+                  content: "",
+
+                  price: 620000
+
+                  
+
+         },
+
+         {
+
+                  id: 1,
+
+                  title: "MM102 M04(BL)",
+
+                  imgUrl: "/img/data2/11001_MM102_M04_BL__3_1.avif",
+
+                  content: "",
+
+                  price: 550000
+
+         },
+
+         {
+
+                  id: 2,
+
+                  title: "MM103 01(1M)",
+
+                  imgUrl: "/img/data2/11001_MM103_01_M__1.avif",
+
+                  content: "",
+
+                  price: 480000
+
+         },
+
+         {
+
+                  id: 3,
+
+                  title: "MM104 Leather LK1",
+
+                  imgUrl: "/img/data2/11001_MM104_LEATHER_LK11_1_1.avif",
+
+                  content: "",
+
+                  price: 490000
+
+         },
+
+         {
+
+                  id: 4,
+
+                  title: "MM105 Leather LIV1",
+
+                  imgUrl: "/img/data2/11001_MM105_LEATHER_LIV1_1_1.avif",
+
+                  content: "",
+
+                  price: 490000
+
+         },
+
+         {
+
+                  id: 5,
+
+                  title: "MM115 Leather L01",
+
+                  imgUrl: "/img/data2/11001_MM106_LEATHER_LIV1_1_1.avif",
+
+                  content: "",
+
+                  price: 448000
+
+         },
+
+         {
+
+                  id: 6,
+
+                  title: "MM111 L2",
+
+                  imgUrl: "/img/data2/11001_MM106_LEATHER_LIV1_1_1.avif",
+
+                  content: "",
+
+                  price: 368000
+
+         },
+
+         {
+
+                  id: 7,
+
+                  title: "MM113 01",
+
+                  imgUrl: "/img/data2/11001_MM108_LEATHER_LBL1_1_1.avif",
+
+                  content: "",
+
+                  price: 368000
+
+         },
+
+         {
+
+                  id: 8,
+
+                  title: "MM109 01",
+
+                  imgUrl: "/img/data2/11001_MM109_01_1_1.avif",
+
+                  content: "",
+
+                  price: 368000
+
+         },
+
+         {
+
+                  id: 9,
+
+                  title: "MM107 G10",
+
+                  imgUrl: "/img/data2/11001_MM111_L2_1_1.avif",
+
+                  content: "",
+
+                  price: 378000
+
+         },
+
+         {
+
+                  id: 10,
+
+                  title: "MM114 BRC9",
+
+                  imgUrl: "/img/data2/11001_MM112_C1_1_1.avif",
+
+                  content: "",
+
+                  price: 368000
+
+         },
+
+         {
+
+                  id: 11,
+
+                  title: "MM112 C1",
+
+                  imgUrl: "/img/data2/11001_MM113_01_1_1.avif",
+
+                  content: "",
+
+                  price: 368000
+
+         },
+
+         {
+
+                  id: 12,
+
+                  title: "MM106 Leather LIV1",
+
+                  imgUrl: "/img/data2/11001_MM114_BRC9_1_1.avif",
+
+                  content: "",
+
+                  price: 448000
+
+         },
+         
+         {
+
+                  id: 13,
+
+                  title: "MM108 Leather LBL1",
+
+                  imgUrl: "/img/data2/11001_MM115_LEATHER_L01_1_1.avif",
+
+                  content: "",
+
+                  price: 448000
+
+         },
+
+         {
+
+                  id: 14,
+
+                  title: "MM116 01",
+
+                  imgUrl: "/img/data2/11001_MM116_01_1_1.avif",
+
+                  content: "",
+
+                  price: 345000
+
+         },
+
+         {
+
+                  id: 15,
+
+                  title: "MM117 C1",
+
+                  imgUrl: "/img/data2/11001_MM117_C1_1_3.avif",
+
+                  content: "",
+
+                  price: 345000
+
+         },
+
+]
+
+
+
+export default data2;
